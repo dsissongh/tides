6379de2659c2ecf5d5bd473bacfa5db381677e84
David Sisson <davidsissonemail@gmail.com>
2017-02-15 21:14:13 -0800

    Pulling tides table specifically

--- tides/main.py
+++ tides/main.py
@@ -1,5 +1,16 @@
 import requests
 
-req = requests.get("http://tides.mobilegeographics.com/locations/5016.html?y=2017&m=2&d=14")
+req = requests.get("http://tides.mobilegeographics.com/locations/5016.html?y=2017&m=2&d=15")
 
-print(str(req.text))
+#print(str(req.text))
+lines = req.text.split("\n")
+printit = False
+for line in lines:
+	if '<pre class="predictions-table">' in line:
+		printit = True
+		
+	if '</pre>' in line:
+		printit = False
+		
+	if(printit):
+		print(line)
