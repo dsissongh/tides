4ca5e31f3096cc467dad810fdf2859fdfb82c521
David Sisson <davidsissonemail@gmail.com>
2017-02-16 23:42:02 -0800

    Pulls High and Low tide and calculates exchange

--- tides/main2.py
+++ tides/main2.py
@@ -3,8 +3,19 @@ tides = 'tides-dabob.txt'
 tides = open(tides,'r')
 
 for tide in tides:
-	if '2017/02/18' in tide:
+	if '2017/02/19' in tide:
 		elements = tide.split()
 		if 'AM' in elements[3]:
-			print(tide.strip())
-			#print(str(elements))
+			if 'L' in elements[6]:
+				low = float(elements[4])
+				
+			if 'H' in elements[6]:
+				high = float(elements[4])
+				
+			print(tide.strip())	
+				
+			
+				
+exchange = high - low
+#print(str(elements))
+print("Exchange: %.2f" % exchange)
